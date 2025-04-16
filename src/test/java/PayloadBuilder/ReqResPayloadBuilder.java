9a2724a0353340f7cd4bcd534913c4c31b9054c0
Madela06 <Brianmadela2@gmail.com>
2025-04-16 20:30:24 +0200

    Added Patch Employee

--- src/test/java/PayloadBuilder/ReqResPayloadBuilder.java
+++ src/test/java/PayloadBuilder/ReqResPayloadBuilder.java
@@ -6,11 +6,21 @@ import org.testng.annotations.Test;
 
 public class ReqResPayloadBuilder {
 
-    public static JSONObject createEmployeeResponse(){
+    public static JSONObject createEmployeeResponse() {
 
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("name","Sibu");
-        jsonObject.put("job","Engineer");
+        jsonObject.put("name", "Sibu");
+        jsonObject.put("job", "Engineer");
+
+        return jsonObject;
+    }
+
+    public static JSONObject PatchEmployeeResponse() {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "Ntsako");
+        jsonObject.put("job", "Senior Engineer");
+        jsonObject.put("Department", "IT");
 
         return jsonObject;
     }
