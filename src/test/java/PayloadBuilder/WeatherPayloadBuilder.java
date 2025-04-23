47c863298f04c4fdd2b648b5eb21b56cf22e00aa
Madela06 <Brianmadela2@gmail.com>
2025-04-23 13:22:47 +0200

    Added WeatherPayloadBuilder

--- src/test/java/PayloadBuilder/WeatherPayloadBuilder.java
+++ src/test/java/PayloadBuilder/WeatherPayloadBuilder.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONObject;
 
 public class WeatherPayloadBuilder {
     public static JSONObject createWeatherPayload() {
+
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("q", "San Francisco Test Station");
         jsonObject.put("appid", "25267eac5de7343f868234b55039d9fc"); // Replace with your actual API key
