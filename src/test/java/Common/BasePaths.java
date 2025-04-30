9fb32c7874398c3508292b1741502d32225235e0
Madela06 <Brianmadela2@gmail.com>
2025-04-30 14:04:51 +0200

    Added Base paths

---
+++ src/test/java/Common/BasePaths.java
@@ -0,0 +1,6 @@
+package common;
+
+public class BasePaths {
+    public static String reqRes_baseUrl = "https://reqres.in";
+    public static String openWeather_baseUrl = "https://api.openweathermap.org";
+}
