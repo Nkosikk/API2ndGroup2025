db48b6401aadd74d9044d29978f498ec6dc94200
Masha <mabitselama@gmail.com>
2025-04-30 12:16:59 +0200

    update

--- src/test/java/RequestBuilder/WeatherStationRequestBuilder.java
+++ src/test/java/RequestBuilder/WeatherStationRequestBuilder.java
@@ -12,9 +12,52 @@ public class WeatherStationRequestBuilder {
 
         return given().contentType("application/json")
                 .when()
+               .queryParams("appid", "b1589ee5727295072e2272d60dfc6904")// Add the API key as a query parameter
                 .body(createWeatherStationPayload())//this is  calling the method in the builder class
                 .log().all()
-                .get(WeatherStation_baseUrl+"/data/3.0/stations")//the base url variable  called from Base
+                .post(WeatherStation_baseUrl+"/data/3.0/stations")//the base url variable  called from Base
+                .then()
+                .log().all()
+                .extract().response();
+
+    }
+
+    public static Response updateWeatherStationRequest() {
+
+        return given().contentType("application/json")
+                .when()
+                .queryParams("appid", "b1589ee5727295072e2272d60dfc6904")
+                .body(createWeatherStationPayload())//this is  calling the method in the builder class
+                .log().all()
+                .post(WeatherStation_baseUrl+"/data/3.0/stations")//the base url variable  called from Base
+                .then()
+                .log().all()
+                .extract().response();
+
+    }
+
+    public static Response patchWeatherStationRequest() {
+
+        return given().contentType("application/json")
+                .when()
+                .queryParams("appid", "b1589ee5727295072e2272d60dfc6904")
+                .body(createWeatherStationPayload())//this is  calling the method in the builder class
+                .log().all()
+                .post(WeatherStation_baseUrl+"/data/3.0/stations")//the base url variable  called from Base
+                .then()
+                .log().all()
+                .extract().response();
+
+    }
+
+    public static Response deleteWeatherStationRequest() {
+
+        return given().contentType("application/json")
+                .when()
+                .queryParams("appid", "b1589ee5727295072e2272d60dfc6904")
+                .body(createWeatherStationPayload())//this is  calling the method in the builder class
+                .log().all()
+                .post(WeatherStation_baseUrl+"/data/3.0/stations")//the base url variable  called from Base
                 .then()
                 .log().all()
                 .extract().response();
---
+++ src/test/java/Tests/Weather/WeatherStation.java
@@ -0,0 +1,50 @@
+package Tests.Weather;
+
+import RequestBuilder.ReqResRequestBuilder;
+import RequestBuilder.WeatherStationRequestBuilder;
+import jdk.jfr.Description;
+import org.testng.annotations.Test;
+
+
+//the @Test is placed at class level not individual method level so that everything is executed
+@Test
+public class WeatherStation {
+
+    @Description("As an API user, I want to send a request to create a new weather station and verify the response")
+    public void createWeatherStationTest(){
+        WeatherStationRequestBuilder.createWeatherStationRequest()// we need to do assertions/validations
+                .then()//keyword for validation
+                .log().all()
+                .assertThat()
+                .statusCode(201);
+    }
+
+    @Description("As an api user, I want to send put request that will update station name")
+    @Test(dependsOnMethods = "createWeatherStationTest")
+    public void updateWeatherStationTest(){
+        WeatherStationRequestBuilder.updateWeatherStationRequest()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Description("As an api user, I want to send patch request that will update employee")
+    @Test(dependsOnMethods = "updateWeatherStationTest()")
+    public void patchWeatherStationTest(){
+        WeatherStationRequestBuilder.patchWeatherStationRequest()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+    @Description("As an api user, I want to send delete request that will delete employee")
+    @Test(dependsOnMethods = "patchWeatherStationTest()")
+    public void deleteWeatherStationTest(){
+        WeatherStationRequestBuilder.deleteWeatherStationRequest()
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(204);
+    }
+}
