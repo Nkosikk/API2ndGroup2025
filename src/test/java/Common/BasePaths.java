18dfa54620fbb3870f1ecad4f3d59000e222f6de
lothando <lothandokhumalo@gmail.com>
2025-05-05 15:59:06 +0200

    Updates from the main branch

--- src/test/java/Common/BasePaths.java
+++ src/test/java/Common/BasePaths.java
@@ -4,6 +4,7 @@ public class BasePaths {
 
 
     public static String reqRes_baseUrl = "https://reqres.in";
+    public static String dog_baseUrl = "https://dog.ceo/api/";
 
     public static String openWeather_baseUrl = "http://api.openweathermap.org";
 
