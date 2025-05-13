540cfb725adf088fe5ee70db0c603398a61cecf5
TaniaD <dikwenit@gmail.com>
2025-05-14 00:31:11 +0200

    Added notes to my code

--- src/test/java/Common/BasePaths.java
+++ src/test/java/Common/BasePaths.java
@@ -3,10 +3,13 @@ package Common;
 public class BasePaths {
 
 
+    //Base URL for the ReqRes API, which provides a set of dummy Restful API endpoints for testing and prototyping
     public static String reqRes_baseUrl = "https://reqres.in";
 
+    //Base URL for the OpenWeather API, used to fetch weather data for various locations.
     public static String openWeather_baseUrl = "http://api.openweathermap.org";
 
+    //Base URL for the RestCountries API, which provides information about countries, such as names, regions, and more.
     public static String restCountries_baseUrl = "https://restcountries.com/v3.1";
 
 
